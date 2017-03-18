deade19b424008d6264f4db87d49c25f10e6998f
Anjan Momi <anjan@momi.ca>
2017-03-18 15:23:08 -0700

    made graph less noisy

--- Final.m
+++ Final.m
@@ -5,8 +5,8 @@
 % getOldVelocity(2) = y velocity only
 % getOldVelocity(3) = magnitude 
 
-% setTheta(theta)
-% getTheta()
+% setTheta(theta) %%setting the angle
+% getTheta() %%gets the theta value
 
 clc
 clear all
@@ -14,28 +14,40 @@ close all
 
 global GRAVITY BALLRADIUS INERTIA DELTA_TIME MASS
 GRAVITY = -9.81;
-BALLRADIUS = 2;
-INERTIA = 3; 
+BALLRADIUS = 1;
+MASS = 1;
+INERTIA = (2/5)*MASS*BALLRADIUS^2; 
 DELTA_TIME = 0.01;
-MASS = 3;
 
-setOldVelocity(0.001,0.001);
+%initial velocity
+setOldVelocity(1,1);
+%ramp angle
 setTheta(pi/4);
 
+
 oldPosition = [0,0];
-on = 1
-while on == 1
+on = 1; %gets out of the while loop
+n = 1; %timer counter
+while on == 1;
 [position, finalVelocity] = slopeNoSlipping;
 oldPosition = position + oldPosition;
-plot(oldPosition(1),oldPosition(2),'O');
-hold on;
-axis([0,36,-36,0]);
+plot(oldPosition(1),oldPosition(2),'o');
+
+%setting graph size
+xyMax = 36;
+axis([0,xyMax,-xyMax,0]);
 pause(DELTA_TIME);
 
-setOldVelocity(finalVelocity(1),finalVelocity(2))
-velocity = getOldVelocity(0)
-
-if oldPosition(1)>36
+%timer counter
+n = n+1;
+%keeping track of position and velocity
+setOldVelocity(finalVelocity(1),finalVelocity(2));
+velocity = getOldVelocity(0);
+%exiting the while loop and displying final vectors
+if abs(oldPosition(1))>xyMax |oldPosition(1)<-1 ||oldPosition(2)> 1 | abs(oldPosition(2))>xyMax
+    final_Position = oldPosition
+    final_Velocity = velocity
+    timer = n*DELTA_TIME
     on = 0;
 end
 end
