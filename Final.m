a054875f4d03806cedf0660f8a16355169223d98
David Cao <davidcao369@hotmail.com>
2017-03-19 00:52:07 -0700

    added more labels

--- Final.m
+++ Final.m
@@ -48,11 +48,12 @@ while on == 1;
     
     %Position Simulation
     subplot(3,1,1)
+    hold on;
     plot(oldPosition(1),oldPosition(2),'o');
     axis([0,xyMax,-xyMax,0]);
     T_disp = timer; 
-    title('Velocity vs Time')
-    xlabel(['Time (s)       ''Current Time: ' num2str(T_disp) 's'])
+    title(['Velocity vs Time,    ',num2str(DELTA_TIME),'s per increment'])
+    xlabel(['Time (s)       ' 'Current Time: ' num2str(T_disp) 's'])
     ylabel('y position')
     
     %Velocity Simulation
@@ -61,15 +62,16 @@ while on == 1;
     hold on;
     V_disp = norm(finalVelocity);
     title('Velocity vs Time')
-    xlabel(['Time (s)       ''Current Velocity: ' num2str(V_disp) 'm/s'])
+    xlabel(['Time (s)       ' 'Current Velocity: ' num2str(V_disp) 'm/s'])
     ylabel('Velocity (m/s)')
     
     %Acceleration Simulation
     subplot(3,1,3)
     hold on;
-    plot(n*DELTA_TIME,(norm(finalVelocity)-getOldVelocity(3))/DELTA_TIME,'o')
+    A_Disp = (norm(finalVelocity)-getOldVelocity(3))/DELTA_TIME
+    plot(n*DELTA_TIME,A_Disp,'o')
     title('Acceleration vs Time')
-    xlabel('Time (s)')
+    xlabel(['Time (s)       ' 'Current Acceleration: ' num2str(A_Disp) 'm/s^2'])
     ylabel('Acceleration (m/s^2)')
     
     pause(DELTA_TIME);
