9c7271f19785894bcf2f324fdc10dc0a84728c1f
Anjan Momi <anjan@momi.ca>
2017-03-24 14:17:20 -0700

    fixed plot function

--- main.m
+++ main.m
@@ -10,7 +10,7 @@ INERTIA = (2/5)*MASS*BALLRADIUS^2;
 
 
 DELTA_TIME = 0.01; % Smaller DELTA time increases precision of slope calculation
-PLOT_TIME = 0.1; % Plot every ___ Seconds
+PLOT_TIME = 0.01; % Plot every ___ Seconds
 
 velocity = [0.01, -0.01];
 time = 0;
@@ -20,6 +20,7 @@ addpath './curves'
 addpath './objects'
 
 [x_positions, y_positions, velocities, accelerations, time, finalPosition] = slope(velocity, pi/4, position, time, [0.916, -0.916])
+plotFunction(x_positions, y_positions, velocities, accelerations, time, finalPosition)
 
 Time_taken = time;
 
