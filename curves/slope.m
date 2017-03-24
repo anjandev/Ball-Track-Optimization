d521a274563290a217fe83aad4a4d9e3881d28a3
David Cao <davidcao369@hotmail.com>
2017-03-23 21:14:14 -0700

    stuck with error

--- curves/slope.m
+++ curves/slope.m
@@ -1,30 +1,40 @@
-function [positions, velocities, accelerations, final_time] = slope(initial_velocity, slope_theta, initial_position, initial_time, final_position)
-    
-    global GRAVITY BALLRADIUS INERTIA MASS DELTA_TIME PLOT_TIME
 
-    % SLOPE 
-    % Function takes in initial parameters, generates a curve and outputs
-    % final calculations (initial parameters + change after going through curve) 
-    % and vectors containing velocities, accelerations, positions at each time
-    % interval
+function [positions, velocities, accelerations, final_time] = slope(initial_velocity, slope_theta, initial_position, initial_time, final_position)
+
+global GRAVITY BALLRADIUS INERTIA MASS DELTA_TIME PLOT_TIME
+
+% SLOPE
+% Function takes in initial parameters, generates a curve and outputs
+% final calculations (initial parameters + change after going through curve)
+% and vectors containing velocities, accelerations, positions at each time
+% interval
+
+% TODO: OUTPUT THE positions, velocities, accelerations
+
+
+setOldVelocity(initial_velocity(1), initial_velocity(2));
+oldPosition = initial_position;
+setTheta(slope_theta)
+
+timer = initial_time;
+
+n = 1;
+while oldPosition(2) > final_position
+    [position, finalVelocity] = slopeNoSlipping((getOldVelocity(2))*DELTA_TIME,1);
+    oldPosition = position + oldPosition;
+    timer = DELTA_TIME + timer;
+    positions(n,1) = oldPosition(1);
+    positions(n,2) = oldPosition(2);
+    velocities(n) = norm(finalVelocity);
+    accelerations(n) = norm(finalVelocity)/DELTA_TIME;
+    n = n + 1;
+    setOldVelocity(finalVelocity(1), finalVelocity(2));
     
-    % TODO: OUTPUT THE positions, velocities, accelerations
-    
-    setTheta(slope_theta);
-    setOldVelocity(initial_velocity(1), initial_velocity(2));
-    
-    oldPosition = initial_position;
-    
-    timer = initial_time;
-        
-    while oldPosition(2) > final_position
-        [position, finalVelocity] = slopeNoSlipping((getOldVelocity(2))*DELTA_TIME,1);
-        oldPosition = position + oldPosition;
-        timer = DELTA_TIME + timer;
-    end
-    
-    final_time = timer;
-    
+end
+
+final_time = timer;
 
 end
+
+
 
--- main.m
+++ main.m
@@ -7,18 +7,22 @@ GRAVITY = -9.81;
 BALLRADIUS = 1;
 MASS = 1;
 INERTIA = (2/5)*MASS*BALLRADIUS^2; 
-DELTA_TIME = 0.01; % Smaller DELTA time increases precision of slope calculation
+DELTA_TIME = 51; % Smaller DELTA time increases precision of slope calculation
 PLOT_TIME = 2; % Plot every ___ Seconds
 
 velocity = [0.01, -0.01];
 time = 0;
-position = [0, 0];
+position = [1, 1];
 
-addpath ./curves
-addpath ./objects
+%addpath './curves'
+%addpath './objects'
 
-[final_position, velocities, accelerations, time] = brachistochrone(velocity, 14, position, time);
+%[final_position, velocities, accelerations, time] = brachistochrone(velocity, 14, position, time);
 
+%[positions, velocities, accelerations, final_time] = slope(initial_velocity, slope_theta, initial_position, initial_time, final_position)
+[positions, velocities, accelerations, final_time] = slope(velocity, 1, position , time, 1)
+
+%x = positions + velocities + accelerations + final_time
 Time_taken = time
 disp('Final Velocity (m/s)')
 disp('   V_x      V_y')
