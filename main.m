4817cc3f5483f39ccd005c7b94427dc1380a535c
David Cao <davidcao369@hotmail.com>
2017-04-03 14:36:03 -0700

    took the magnitude for angular acceleration and velocity, chaged impulse time

--- main.m
+++ main.m
@@ -5,8 +5,8 @@ clc
 global GRAVITY BALLRADIUS INERTIA MASS DELTA_TIME PLOT_TIME STATIC_FRICTION KINETIC_FRICTION
 
 % Kinetic friction is less thann static friction
-KINETIC_FRICTION = 0.3;
-STATIC_FRICTION = 0.4;
+KINETIC_FRICTION = 0.2;
+STATIC_FRICTION = 0.3;
 
 GRAVITY = -9.81;
 BALLRADIUS = 0.007804912;
@@ -25,7 +25,7 @@ position = [INCH_TO_METER, -INCH_TO_METER];
 omega = 0;
 slope_angle = pi/6;
 final_slope_position = [0, 3];
-energy_remain = 0.12;
+energy_remain = 0.10;
 
 
 addpath './curves'
@@ -39,19 +39,19 @@ addpath './objects'
 
 [x_positions2, y_positions2, velocities2, accelerations2, time, position, omegas2, alphas2, omega] = slope(getOldVelocity(0), position, time, [position(1)+ 2*INCH_TO_METER, position(2)], omega);
 
-[x_positions3, y_positions3, velocities3, accelerations3, time, position, omegas3, alphas3, omega] = slope(getOldVelocity(0), position, time, [position(1)+ 7.5*INCH_TO_METER, position(2) + 3*INCH_TO_METER], omega);
+[x_positions3, y_positions3, velocities3, accelerations3, time, position, omegas3, alphas3, omega] = slope(getOldVelocity(0), position, time, [position(1)+ 8*INCH_TO_METER, position(2) + 3*INCH_TO_METER], omega);
 
-[omega,impulse_acceleration] = bounce(energy_remain, getOldVelocity(0));
+[omega,impulse_acceleration,time] = bounce(energy_remain, getOldVelocity(0), time);
 
-[x_positions4, y_positions4, velocities4, accelerations4, time, omegas4, alphas4] = free(getOldVelocity(0), time, position,position(1)+ 1*INCH_TO_METER, position(2) + 0.2*INCH_TO_METER,-0.889);
+[x_positions4, y_positions4, velocities4, accelerations4, time, omegas4, alphas4] = free(getOldVelocity(0), time, position,position(1)+ 1*INCH_TO_METER, position(2) + 0.35*INCH_TO_METER,-0.889);
 accelerations4(1) = impulse_acceleration;
 
 x_positions = [x_positions x_positions1 x_positions2 x_positions3,x_positions4];
 y_positions = [y_positions y_positions1 y_positions2 y_positions3,y_positions4];
 velocities = [velocities velocities1 velocities2 velocities3,velocities4];
 accelerations = abs([accelerations accelerations1 accelerations2,accelerations3,accelerations4]);
-omegas = [omegas omegas1 omegas2 omegas3,omegas4];
-alphas = [alphas alphas1 alphas2 alphas3,alphas4];
+omegas = abs([omegas omegas1 omegas2 omegas3,omegas4]);
+alphas = abs([alphas alphas1 alphas2 alphas3,alphas4]);
 
 plotFunction(x_positions, y_positions, velocities, accelerations, time, alphas, omegas, position);
 
